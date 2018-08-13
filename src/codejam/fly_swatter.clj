18bb3d8fadd5c09095fa3d65d9a164610954423e
Sidhant Godiwala <sidhant.godiwala@booking.com>
2018-08-13 22:20:00 +0200

    Fly Swatter
    Completed

--- src/codejam/fly_swatter.clj
+++ src/codejam/fly_swatter.clj
@@ -1,7 +1,128 @@
 (ns codejam.fly-swatter
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [swissknife.core :refer [map-values]]))
+
+
+(s/def ::fly pos?)
+(s/def ::racquet pos?)
+(s/def ::thickness pos?)
+(s/def ::chord pos?)
+(s/def ::gap pos?)
+(s/def ::position (s/tuple double? double?))
+(s/def ::problem (s/tuple ::fly ::racquet ::thickness ::chord ::gap))
+(s/def ::probability (fn [x] (<= 0 x 1)))
+
+
+(defn within-racquet?
+  "The fly's centre is inside the outside racquet radius"
+  [racquet [x y]]
+  (<= (+ (Math/pow x 2)
+         (Math/pow y 2))
+      (Math/pow racquet 2)))
+
+
+(s/fdef within-racquet?
+        :args (s/cat ::racquet ::racquet
+                     ::position ::position)
+        :ret boolean?)
+
+
+(defn hit-by-rim?
+  "Given a position within the racquet, returns if the fly
+  was hit by the rim"
+  [racquet thickness fly [x y]]
+  (<= (- racquet thickness)
+      (+ (Math/sqrt (+ (Math/pow x 2)
+                       (Math/pow y 2)))
+         fly)))
+
+
+(s/fdef hit-by-rim?
+        :args (s/cat ::racquet ::racquet
+                     ::thickness ::thickness
+                     ::fly ::fly
+                     ::position ::position)
+        :ret boolean?)
+
+
+(defn hit-by-chord?
+  "Given a position within the racquet, returns if the fly
+  was hit by a chord"
+  [chord gap fly [x y]]
+  (let [closest-x (- (Math/abs x) fly)
+        closest-y (- (Math/abs y) fly)
+        shifted-x (- closest-x chord)
+        shifted-y (- closest-y chord)
+        space-x (rem shifted-x (+ gap (* 2 chord)))
+        space-y (rem shifted-y (+ gap (* 2 chord)))]
+    (or (<= closest-x chord)
+        (<= closest-y chord)
+        (zero? space-x)
+        (zero? space-y)
+        (< gap (+ (* 2 fly) space-x))
+        (< gap (+ (* 2 fly) space-y)))))
+
+
+(s/fdef hit-by-chord?
+        :args (s/cat ::chord ::chord
+                     ::gap ::gap
+                     ::fly ::fly
+                     ::position ::position)
+        :ret boolean?)
+
+
+(defn hit-by-racquet?
+  "combines `hit-by-rim?` and `hit-by-chord?`"
+  [racquet thickness chord gap fly position]
+  (or (hit-by-rim? racquet thickness fly position)
+      (hit-by-chord? chord gap fly position)))
+
+
+(s/fdef hit-by-racquet?
+        :args (s/cat ::racquet ::racquet
+                     ::thickness ::thickness
+                     ::chord ::chord
+                     ::gap ::gap
+                     ::fly ::fly
+                     ::position ::position))
+
+
+(defn generate-position!
+  "returns a randomly generated position [x y] within the
+  bounding box of the racquet"
+  [racquet]
+  [(* (rand racquet)
+      (rand-nth [-1 1]))
+   (* (rand racquet)
+      (rand-nth [-1 1]))])
+
+
+(s/fdef generate-position!
+        :args (s/cat ::racquet ::racquet)
+        :ret ::position)
+
+
+(defn probability
+  "returns the probability of the fly being hit by
+  the racquet, `num-samples` is the number of samples simulated"
+  [num-samples fly racquet thickness chord gap]
+  (let [tally (->> (repeatedly num-samples (partial generate-position! racquet))
+                   (filter (partial within-racquet? racquet))
+                   (map (partial hit-by-racquet? racquet thickness chord gap fly))
+                   (frequencies))]
+    (double (/ (get tally true 0)
+               (+ (get tally true 0) (get tally false 0))))))
+
+
+(s/fdef probability
+        :args (s/cat ::num-samples pos-int?
+                     ::fly ::fly
+                     ::racquet ::racquet
+                     ::thickness ::thickness
+                     ::chord ::chord
+                     ::gap ::gap))
 
 
 (defn parse-line
@@ -13,17 +134,6 @@
      (Double/parseDouble c)
      (Double/parseDouble g)]))
 
-
-(s/def ::fly pos?)
-(s/def ::raquet pos?)
-(s/def ::thickness pos?)
-(s/def ::chord pos?)
-(s/def ::gap pos?)
-(s/def ::problem (s/tuple ::fly
-                          ::raquet
-                          ::thickness
-                          ::chord
-                          ::gap))
 
 (s/fdef parse-line
         :args (s/cat ::line string?)
@@ -38,6 +148,29 @@
          (map parse-line)
          (doall))))
 
+
 (s/fdef read-input
         :args (s/cat ::f io/reader)
         :ret (s/coll-of ::problem))
+
+
+(defn solve
+  [problems]
+  (letfn [(mapper [problem]
+            (apply probability 1000000 problem))]
+
+    (map mapper problems)))
+
+
+(s/fdef solve
+        :args (s/cat ::problems (s/coll-of ::problem))
+        :ret (s/coll-of ::probability))
+
+
+(defn write-output!
+  [f solutions]
+  (letfn [(formatter [idx sol]
+            (format "Case #%d: %f" idx sol))]
+    (->> (map formatter (drop 1 (range)) solutions)
+         (str/join "\n")
+         (spit f))))
--- test/codejam/fly_swatter_test.clj
+++ test/codejam/fly_swatter_test.clj
@@ -11,6 +11,84 @@
 
 
 (facts
+ "within racquet?"
+
+ (within-racquet? 5.0 [3.0 5.0])
+ => false
+
+ (within-racquet? 5.0 [3.0 4.0])
+ => true
+
+ (within-racquet? 5.0 [3.0 3.0])
+ => true)
+
+
+(facts
+ "Hit by rim?"
+
+ (hit-by-rim? 6.0 0.5 0.4 [3.0 4.0])
+ => false
+
+ (hit-by-rim? 6.0 0.5 0.5 [3.0 4.0])
+ => true
+
+ (hit-by-rim? 6.0 0.5 0.6 [3.0 4.0])
+ => true)
+
+
+(facts
+ "Hit by chord?"
+
+ (hit-by-chord? 1.0 2.0 0.1 [0.5 0.5])
+ => true
+
+ (hit-by-chord? 1.0 2.0 0.1 [1.1 1.1])
+ => true
+
+ (hit-by-chord? 1.0 2.0 0.1 [5.1 5.1])
+ => true
+
+ (hit-by-chord? 1.0 2.0 0.1 [6.9 6.9])
+ => true
+
+ (hit-by-chord? 1.0 2.0 0.1 [6.0 6.0])
+ => false)
+
+
+(facts
+ "Hit by racquet"
+
+ (hit-by-racquet? 6.0 0.5 1.0 1.0 0.5 [3.0 4.0])
+ => true)
+
+
+(facts
+ "generate position!"
+
+ (generate-position! 2.0)
+ => [-0.5 1.0]
+
+ (provided
+  (rand 2.0) =streams=> [0.5 1.0]
+
+  (rand-nth [-1 1]) =streams=> [-1 1]))
+
+
+(facts
+ "probability"
+
+ (probability 5 0.1 6.0 0.5 1.0 1.0)
+ => 0.75
+
+ (provided
+  (generate-position! 6.0) =streams=> [[5.9 5.9]
+                                       [0.0 0.0]
+                                       [1.0 1.0]
+                                       [2.0 2.0]
+                                       [1.5 1.5]]))
+
+
+(facts
  "parse line"
 
  (parse-line "0.25 1.0 0.1 0.01 0.5")
@@ -44,3 +122,21 @@
      [1.0E-5 10000.0 1.0E-5 1.0E-5 1000.0]
      [0.4 10000.0 1.0E-5 1.0E-5 700.0]
      [1.0 100.0 1.0 1.0 10.0]])
+
+
+(facts
+ "solve"
+
+ (solve [[0.25 1.0 0.1 0.01 0.5]
+         [0.25 1.0 0.1 0.01 0.9]
+         [0.00001 10000 0.00001 0.00001 1000]
+         [0.4 10000 0.00001 0.00001 700]
+         [1 100 1 1 10]])
+ => [1.000 0.910 0.000 0.002 0.574]
+
+ (provided
+  (probability 1000000 0.25 1.0 0.1 0.01 0.5) => 1.000
+  (probability 1000000 0.25 1.0 0.1 0.01 0.9) => 0.910
+  (probability 1000000 0.00001 10000 0.00001 0.00001 1000) => 0.000
+  (probability 1000000 0.4 10000 0.00001 0.00001 700) => 0.002
+  (probability 1000000 1 100 1 1 10) => 0.574))
